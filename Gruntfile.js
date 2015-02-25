ae3ce1eb7ea6ce149908166f2c15fa6bfea6381b
Adam Knight <adam@revolutionarysystems.co.uk>
2015-02-25 11:19:36 +0000

    Added dist task and update grunt-haven

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,6 +9,7 @@ module.exports = function(grunt) {
 
   // Tasks  
 
+  grunt.registerTask('dist', ['haven:update']);
   grunt.registerTask('deploy', ['haven:deploy']);
   grunt.registerTask('ci', ['haven:deployOnly']);
 
